1e51dfc37575960f4524e7ca27af0f32baeca7fe
Amalrajambali <amalrajambali123@gmail.com>
2023-04-05 11:48:16 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import BookmarkPage from "./components/pages/BookmarkPage";
+import HomePage from "./components/pages/HomePage";
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <Router>
+        <Header/>
+        <Routes>
+           <Route path="/" element={<HomePage/>} />
+           <Route path="/bookmark" element={<BookmarkPage/>}/>
+          </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/BookmarkPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bookmark from '../Bookmark'
+
+function BookmarkPage() {
+  return (
+    <div>
+        <Bookmark/>
+    </div>
+  )
+}
+
+export default BookmarkPage
